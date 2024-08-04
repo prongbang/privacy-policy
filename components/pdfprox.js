c165dc6d81845ddeb21084b9f029b93c77cd2185
prongbang <prongbang@gmail.com>
2024-08-04 15:05:38 +0700

    feat: add ads

--- components/pdfprox.js
+++ components/pdfprox.js
@@ -2,13 +2,16 @@ const PdfProX = { template: `
 <div>
     <strong>Privacy Policy</strong>
     <p>
-      OTEL TECH & INTENIQUETIC built the PDF Pro X app as a free service. This application is provided by OTEL TECH & INTENIQUETIC at no cost and is intended for use as-is.
+      INTENIQUETIC built the PDF Pro X app as a free service. This application is provided by INTENIQUETIC at no cost and is intended for use as-is.
     </p>
     <p>
       This page informs users about our policies regarding the collection, use, and disclosure of personal information if anyone decides to use our service.
     </p>
     <p>
       Please note that PDF Pro X does not collect or use any personal information. All data is managed locally on your device, and we do not share or transmit any information to external parties. 
+    </p>
+    <p>
+      To support the free availability of PDF Pro X, we may display advertisements within the app. These ads are managed by third-party ad networks. While we do not collect your personal information, these ad networks may use their own tracking technologies to serve and manage ads. We do not have access to or control over their data collection practices.
     </p>
     <p>
       The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which can be accessed within the PDF Pro X app unless otherwise defined in this Privacy Policy.
